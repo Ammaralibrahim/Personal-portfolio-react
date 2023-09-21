1906d786535943f8bda591f24587ab047dae308f
ammar_ib <ammaryasir8088@gmail.com>
2023-09-21 18:14:08 +0300

    used <Link> instead   <a> tags

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,28 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom"; // react-router-dom'dan Link'i içe aktarın
 import Logo from "../Components/images/logo.png";
 import whatsapp from "../Components/images/whatsapp.png";
 import instagram from "../Components/images/instagram.png";
 import facebook from "../Components/images/facebook.png";
 
 function Header() {
-
   return (
     <>
       <nav id="desktop-nav">
-        <div class="logo">
-          <img src={Logo} alt="Reyhana logo" className="logo" />
+        <div className="logo"></div>
+          <Link to="/">
+            <img src={Logo} alt="Reyhana logo" className="logo" />
+          </Link>
         </div>
-        
+
         <div>
-          <ul class="nav-links">
+          <ul className="nav-links">
             <li>
-              <a href="#products">Products</a>
+              <Link to="/product-list">Products</Link>
             </li>
 
             <li>
-              <a href="#essences">Essences</a>
+              <Link to="/essences">Essences</Link>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <Link to="/contact">Contact</Link>
             </li>
             <li>
               <div id="socials-container">
@@ -31,21 +34,21 @@ function Header() {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={whatsapp}  className="icon" alt="whatsapp"/>
+                  <img src={whatsapp} className="icon" alt="whatsapp" />
                 </a>
                 <a
                   href="https://www.instagram.com/reyhana_538?r=nametag"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={instagram}  className="icon" alt="whatsapp" />
+                  <img src={instagram} className="icon" alt="instagram" />
                 </a>
                 <a
                   href="https://www.facebook.com/profile.php?id=100083401821237"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={facebook}  className="icon" alt="whatsapp" />
+                  <img src={facebook} className="icon" alt="facebook" />
                 </a>
               </div>
             </li>
@@ -53,8 +56,8 @@ function Header() {
         </div>
       </nav>
       <nav id="hamburger-nav">
-        <div class="logo">
-          <img src={Logo} alt="Reyhana logo"  className="logo" />
+        <div className="logo">
+          <img src={Logo} alt="Reyhana logo" className="logo" />
         </div>
       </nav>
     </>
