6dd72d270b5b7100fe44cdd54be579fb11ed3187
ammar_ib <ammaryasir8088@gmail.com>
2023-09-23 16:28:09 +0300

    cdscd

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -30,7 +30,7 @@ function Header() {
             <li>
               <div id="socials-container">
                 <a
-                  href="https://api.whatsapp.com/send?phone=+905383654262&text=Can I have more information about your products? Ürünleriniz hakkında daha fazla bilgi sahibi olabilirmiyim? هل يمكنني معرفة المزيد عن منتجاتكم؟"
+                  href="https://api.whatsapp.com/send?phone=+905383654262&text=%D9%87%D9%84%20%D9%8A%D9%85%D9%83%D9%86%D9%86%D9%8A%20%D9%85%D8%B9%D8%B1%D9%81%D8%A9%20%D8%A7%D9%84%D9%85%D8%B2%D9%8A%D8%AF%20%D8%B9%D9%86%20%D9%85%D9%86%D8%AA%D8%AC%D8%A7%D8%AA%D9%83%D9%85%D8%9F%20%C3%9Cr%C3%BCnleriniz%20hakk%C4%B1nda%20daha%20fazla%20bilgi%20sahibi%20olabilir%20miyim%3F%20Can%20I%20have%20more%20information%20about%20your%20products%3F"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
